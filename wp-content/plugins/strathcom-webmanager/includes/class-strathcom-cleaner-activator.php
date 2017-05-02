77a5777c7daecbb4c883cf473b0cba8330919c2e
ıqO <merenuou@gmail.com>
2017-05-02 02:13:25 -0600

    add plugins

---
+++ wp-content/plugins/strathcom-webmanager/includes/class-strathcom-cleaner-activator.php
@@ -0,0 +1,23 @@
+<?php
+// @codingStandardsIgnoreFile
+
+/**
+ *  
+ *
+ * @since      1.0.0
+ * @package    Strathcom_Cleaner
+ * @subpackage Strathcom_Cleaner/includes
+ * @author     Strathcom <obinna@strathcom.com>
+ */
+class Strathcom_Cleaner_Activator {
+
+	/**
+	 *  
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ wp-content/plugins/strathcom-webmanager/uninstall.php
@@ -0,0 +1,16 @@
+<?php
+// @codingStandardsIgnoreFile
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ *
+ * @link       https://github.com/oldmanobi
+ * @since      1.0.0
+ *
+ * @package    Strathcom_Cleaner
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
